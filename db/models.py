71ea5bf55ebb42e81f52b68394057afd887e22e6
voychenko-v <voychenkovitaliy@gmail.com>
2022-07-31 01:52:11 +0300

    create DB and table Exercise

--- db/models.py
+++ db/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import models as auth_models
 
-# Create your models here.
+'''
+documentation fields: https://docs.djangoproject.com/en/4.0/ref/models/fields/
+'''
+
+
+class Exercise(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=False)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(auth_models.User, on_delete=models.CASCADE)
