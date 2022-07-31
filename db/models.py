c410442ca3577f31054cfaff77d26c50e38d4832
vnosinov <nosinov@gmail.com>
2022-07-31 19:20:33 +0300

    nv add new branch and add new table Articles

--- db/models.py
+++ db/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.contrib.auth import models as auth_models
 # Create your models here.
+
+
+class Articles(models.Model):
+    """
+    таблица Articles - соответствует пункту меню Статьи
+
+    """
+    title = models.CharField(max_length=200)
+    intro_article = models.TextField(blank=False)
+    article = models.TextField(blank=False)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(auth_models.User, on_delete=models.CASCADE)
