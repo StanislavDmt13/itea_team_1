4fbead57e3ed587ea16680bb6814e0933c17550b
voychenko-v <voychenkovitaliy@gmail.com>
2022-08-06 16:04:34 +0300

    rebuilt the database and added the user table

--- db/admin.py
+++ db/admin.py
@@ -1,8 +1,51 @@
 from django.contrib import admin
-from db.models import Exercise, Articles
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
+
+from . import models
 
 
-class ExerciseAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = ["email", "username"]
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Personal Info"),
+            {
+                "fields": (
+                    "avatar",
+                    "username",
+                    "first_name",
+                    "last_name",
+                    "birthday",
+                    "phone",
+                )
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "created_at")}),
+    )
+    add_fieldsets = (
+        (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
+    )
+
+    readonly_fields = ("created_at",)
+
+
+class TrainAdmin(admin.ModelAdmin):
 
     list_display = ['title', 'time_create', 'author']
 
@@ -12,5 +55,6 @@ class ArticlesAdmin(admin.ModelAdmin):
     list_display = ['title', 'time_create', 'author']
 
 
-admin.site.register(Exercise, ExerciseAdmin)
-admin.site.register(Articles, ArticlesAdmin)
+admin.site.register(models.Train, TrainAdmin)
+admin.site.register(models.Articles, ArticlesAdmin)
+admin.site.register(models.User, UserAdmin)
--- prosport/settings.py
+++ prosport/settings.py
@@ -109,6 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'db.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
