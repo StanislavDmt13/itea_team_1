989efd8d790374f018d7be88473e6b6c1a961b04
voychenko-v <voychenkovitaliy@gmail.com>
2022-08-25 20:19:44 +0300

    add train page

--- backend/athlete/views.py
+++ backend/athlete/views.py
@@ -13,6 +13,11 @@ from .forms import AthleteEditAvatar, AthleteEditForm
 class AthleteView(LoginRequiredMixin, TemplateView):
     template_name = 'athlete/cabinet.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        kwargs['train'] = Train.objects.all()
+        return context
+
 
 class AthleteEditView(LoginRequiredMixin, UpdateView):
     model = User
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('train/', views.train, name='train'),
+    path('train_page/<int:train_pk>/', views.train_page, name='train-page'),
     path('train-program/', views.train_program, name='train-program'),
 ]
